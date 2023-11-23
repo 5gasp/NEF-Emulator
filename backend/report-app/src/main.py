4b65e65165b28270da7566adf60284feb5983125
Rafael Direito <rdireito@av.it.pt>
2023-11-23 19:12:44 +0000

    Updated report API to consume an env with the location of the report file.

--- backend/report-app/src/main.py
+++ backend/report-app/src/main.py
@@ -8,15 +8,26 @@ from fastapi import Request, FastAPI
 from fastapi.responses import JSONResponse, FileResponse
 import os
 import json
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+
+# Check the report base location
+REPORT_DEFAULT_PATH = os.getenv("REPORT_PATH", "/shared/report.json") 
+REPORT_BASE_PATH = os.path.dirname(REPORT_DEFAULT_PATH)
 
 # On Boot, create the Report File
-if not os.path.exists("../shared/report.json"):
-   with open("../shared/report.json", 'w') as jsonFile:
+logging.debug(f"Is the file '{REPORT_DEFAULT_PATH}' already created? "
+              f"{os.path.exists(REPORT_DEFAULT_PATH)}"
+)
+if not os.path.exists(REPORT_DEFAULT_PATH):
+    logging.debug(f"Will create the file '{REPORT_DEFAULT_PATH}")
+    with open(REPORT_DEFAULT_PATH, 'w') as jsonFile:
         data = []
         json.dump(data, jsonFile, indent=2)
-    
-    
+    logging.debug(f"Was the file '{REPORT_DEFAULT_PATH}' created? "
+              f"{os.path.exists(REPORT_DEFAULT_PATH)}"
+)
 
 app = FastAPI()
 
@@ -30,8 +41,8 @@ def create_report(
     filename: str = 'report.json',
     http_request: Request
 ) -> Any:
-    if not os.path.exists("../shared/" + filename):
-        with open("../shared/" + filename, 'x') as jsonFile:
+    if not os.path.exists(os.path.join(REPORT_BASE_PATH + filename)):
+        with open(os.path.join(REPORT_BASE_PATH + filename), 'x') as jsonFile:
             data = []
             json.dump(data, jsonFile, indent=2)
         return JSONResponse(content=f"Report named {filename} created",status_code=200)
@@ -45,10 +56,10 @@ def get_report(
     http_request: Request
 ) -> Any:
 
-    if not os.path.exists("../shared/" + filename):
+    if not os.path.exists(os.path.join(REPORT_BASE_PATH + filename)):
         return JSONResponse(content="File not Found",status_code=404)
     
-    report_path = os.path.abspath("../shared/" + filename)
+    report_path = os.path.abspath(os.path.join(REPORT_BASE_PATH + filename))
 
     return FileResponse(report_path,filename=filename)
 
@@ -58,7 +69,7 @@ def delete_report(
     filename: str = 'report.json',
     http_request: Request
 ) -> Any:
-    if os.path.exists("../shared/" + filename):
-        os.remove("../shared/" + filename)
+    if os.path.exists(os.path.join(REPORT_BASE_PATH + filename)):
+        os.remove(os.path.join(REPORT_BASE_PATH + filename))
         return JSONResponse(content="Report deleted",status_code=200)
     return JSONResponse(content="File not Found",status_code=404)
